bf86c751568f31c342e03be9f203752e8a0af33b
Leo Font <leo.fontg@gmail.com>
2018-06-20 18:32:48 +0200

    master - Added Primary Bean annotation

--- src/main/java/com/leofont/didemo/controllers/PropertyInjectedController.java
+++ src/main/java/com/leofont/didemo/controllers/PropertyInjectedController.java
@@ -3,6 +3,7 @@ package com.leofont.didemo.controllers;
 import com.leofont.didemo.services.GreetingService;
 import com.leofont.didemo.services.GreetingServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -12,10 +13,12 @@ public class PropertyInjectedController {
     // Is an example of wrong style!!!
 
     @Autowired
+    @Qualifier("greetingServiceImpl")
     public GreetingService greetingServiceImpl;
     // This doesn't require qualifier because Spring uses the name doing reflection to
     // find the corresponding service bean. It´s better to use qualifier to better
     // express intent.
+    // If I put the Qualifier annotation it will override it.
 
     public String sayHello() {
 
--- src/main/java/com/leofont/didemo/services/PrimaryGreetingService.java
+++ src/main/java/com/leofont/didemo/services/PrimaryGreetingService.java
@@ -1,5 +1,14 @@
 package com.leofont.didemo.services;
 
-public class PrimaryGreetingService {
-    
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Service
+@Primary
+public class PrimaryGreetingService implements GreetingService {
+
+    @Override
+    public String sayGreeting() {
+        return "Hello from the primary greeting service";
+    }
 }
